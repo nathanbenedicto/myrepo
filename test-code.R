891d57da13cdf39eb949bed93ad81b666c190ebb
nathanbenedicto <nathaniel.-.benedicto@obf.ateneo.comgit config --listgit config --global user.email nathaniel.-.benedicto@obf.ateneo.com>
2022-06-16 15:17:55 +0800

    Updated introduction for "Linking GitHub and RStudio"

--- test-code.R
+++ test-code.R
@@ -1,4 +1,4 @@
-print("My name is Tantan")
-print("Hello world!")
-print("This is a test")
-print("I am from Davao City and I like cats and durian.")
+print("My name is Nathaniel Benedicto.")
+print("You can call me Tantan.")
+print("I am from Davao City")
+print ("I like cats, and I like to eat durian in all its forms — as a fruit, as a drink, and even as pie.")
